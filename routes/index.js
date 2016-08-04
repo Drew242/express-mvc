8960cfb4ecc7dfc5df958d1426cafda30fecf52e
Drew Conly <atconly@gmail.com>
2016-08-04 10:50:35 -0600

    routes on routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,25 @@
+var API = require('./api'),
+    Dashboard = require('./dashboard');
+
+console.log("API ROUTES:", API);
+
 module.exports = function(app) {
+    app.get('/', function(req, res){
+        res.send('Site homepage');
+    });
 
-  app.get('/', function(req, res) {
-    
-  })
+    // http://localhost:3000
+    app.get('/api', API.get);
 
+    // http://localhost:3000/api/user?id=1
+    app.get('/api/user', API.user);
+
+    // http://localhost:3000/api/users
+    app.get('/api/users', API.users);
+
+    // http://localhost:3000/dashboard
+    app.get('/dashboard', Dashboard.home);
+
+    // http://localhost:3000/dashboard/settings
+    app.get('/dashboard/settings', Dashboard.settings);
 }
--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 var express    = require('express'),
     logger     = require('morgan'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    path = require('path'),
+	  port = process.env.PORT||1337;
 
 // Define App //
 var app = express();
 
 // Mount Middleware //
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded( {extended: true}));
+app.post('*', bodyParser.json(), bodyParser.urlencoded({ extended: true }))
+
+
+// defines static routes & covers Windows OS //
+app.use(express.static(path.join(__dirname, 'public')));
 
 require('./routes')(app)
 
 // Listener //
-var port = process.env.PORT || 8080
 app.listen(port, function() {
   console.log(`Server running on port: ${port}`);
 })
